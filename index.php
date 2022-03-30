af000cf17f15e607cc82cc2ab36821a28a2e6132
rici86 <rici86@yandex.com>
2022-03-30 14:38:17 +0200

    readme

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="A simple calendar for HTML demo purposes. With PHP and CSS grid.">
+    <meta name="description" content="A simple calendar for with PHP and CSS grid. Conceived for static HTML demo purposes, but it could receive data from a DB or other sources.">
     <meta name="author" content="Rici86.com">
 
     <title>Grid calendar</title>
@@ -92,6 +92,14 @@
                 }
             ?>                                
         </div>
+    </div>
+    <div class="readme">
+        <h1>Grid-calendar by <a href="https://www.rici86.com">Rici86</a> </h1>
+        <p>
+            A simple calendar with PHP and CSS grid. It's useful if you need to add a static calendar to a static website for demo purposes, but could also be adapted to receive data from a DB or other sources.
+            <br>The CSS grid .calendar-container displays the main grid, with a top navigation and title, and a secondary grid (no subgrid yet, because of browser support). 
+            <br>The secondary grid contains the weekdays and days of the month. The days immediately before the start of the month or after its end are added with lighter color. PHP is used to cycle each day, and you can add events to a single day in the cycle. You could also add every single day in a static way with HTML divs.
+        </p>
     </div>
 </body>
 </html>
